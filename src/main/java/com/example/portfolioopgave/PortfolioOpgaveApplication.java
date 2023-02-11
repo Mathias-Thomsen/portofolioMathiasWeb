a98cfcbf29eefcd70afae91f642fd7b62ae65f7d
Mathias-Thomsen <math831t6@stud.kea.dk>
2023-02-11 10:44:53 +0100

    First commit

---
+++ src/main/java/com/example/portfolioopgave/PortfolioOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolioopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioOpgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioOpgaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfolioopgave/PortfolioOpgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioOpgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
